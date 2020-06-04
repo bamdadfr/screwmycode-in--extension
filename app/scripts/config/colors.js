b90934d4a554a9dd5cd1e920b663cb6ae02be820
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-06-04 22:53:30 +0200

    javascript injected colors

--- app/scripts/config/colors.js
+++ app/scripts/config/colors.js
@@ -1,13 +1,37 @@
 import { Color, Rainbow } from '../functions/colors'
 import rangeMap from '../functions/rangeMap'
 
-export default {
+const colors = {
     'darkest': 'black',
     'darker': '#808386',
     'normal': null,
     'brighter': 'rgb(222, 106, 99)',
     'brightest': 'rgb(99, 188, 248)',
 }
+
+const theme = {
+    'background': '#242930',
+    'color': '#808386',
+    'border': '#343434',
+    'normal-color': '#63BCF8',
+    'soft-background': '#2F353E',
+}
+
+export default colors
+
+const init = () => {
+
+    const root = document.documentElement
+
+    Object.keys (theme).forEach ((k) => {
+
+        root.style.setProperty ('--' + k, theme[k])
+    
+    })
+
+}
+
+init ()
 
 const color1 = Color (99, 188, 248)
 const color2 = Color (222, 106, 99)
