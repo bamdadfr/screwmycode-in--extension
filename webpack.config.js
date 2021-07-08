de28ac8b5b740f79f0a2285a25aa5feb7b4ff5e5
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-08 10:45:19 +0200

    feat(app): added babel + config + webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -32,4 +32,15 @@ module.exports = {
             ],
         }),
     ],
+    'module': {
+        'rules': [
+            {
+                'test': /\.js$/,
+                'exclude': /node_modules/,
+                'use': {
+                    'loader': 'babel-loader',
+                },
+            },
+        ],
+    },
 }
