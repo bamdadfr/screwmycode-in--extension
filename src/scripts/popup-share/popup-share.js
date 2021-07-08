35e123d741973fddfa1db572257400dad78f46d5
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-08 18:05:11 +0200

    fix(popup-share): `speed` value was fetched onMount and not onClick
    [force-release]

--- src/scripts/popup-share/popup-share.js
+++ src/scripts/popup-share/popup-share.js
@@ -12,16 +12,15 @@ export async function PopupShare () {
 
     const share = document.getElementsByClassName ('smc-share')[0]
     const browser = await Browser ()
-    const { speed } = await State ()
-    const baseUrl = 'https://screwmycode.in/youtube/'
-    const speedUrl = 'speed='
     const id = await UtilsGetId ()
-    const url = `${baseUrl}${id}?${speedUrl}${speed}`
+    const baseUrl = `https://screwmycode.in/youtube/${id}?speed=`
 
-    share.onclick = () => {
+    share.onclick = async () => {
+
+        const { speed } = await State ()
 
         browser.tabs.create ({
-            'url': url,
+            'url': baseUrl + speed,
         })
 
     }
