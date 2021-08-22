20e8792c95cf3359fc393884d297e034fcca5447
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-22 04:34:26 +0200

    perf: remove unnecessary onLoad calls by clearing interval once loaded

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,24 +1,26 @@
 import { handlePlayer } from './player/handle-player'
 import { initializeState } from './state/initialize-state'
-import { onNewHref } from './utils/on-new-href'
 import { isPageWatch } from './utils/is-page-watch'
+import { INTERVAL } from './constants'
 
 let isLoaded = false
+let interval = undefined
 
 window.addEventListener ('load', async () => {
 
     await initializeState ()
 
-    onNewHref (async () => {
+    // repeat loader until player is handled
+    interval = setInterval (async () => {
 
         if (!isPageWatch (window.location.href)) return
 
-        if (isLoaded) return
+        if (isLoaded) return clearInterval (interval)
 
         isLoaded = true
 
         await handlePlayer ()
     
-    })
+    }, INTERVAL)
 
 }, { 'once': true })
